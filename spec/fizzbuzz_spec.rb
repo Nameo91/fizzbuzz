2651bc78055688d3f15eae0208f86a939c40bfd8
eggs-benny <benedict.smith87@gmail.com>
2022-09-05 12:38:49 +0100

    added describe end to test file

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,13 @@
 require 'fizzbuzz'
-it 'returns "Fizz" for the number 3' do
-  expect(3.fizzbuzz).to eq 'Fizz'
-end
-it 'returns "Buzz" for the number 5' do
-  expect(5.fizzbuzz).to eq 'Buzz'
-end 
-it 'returns "Fizzbuzz for the number 15' do
-  expect(15.fizzbuzz).to eq "FizzBuzz"
+
+describe 'fizzbuzz' do
+  it 'returns "Fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq 'Fizz'
+  end
+  it 'returns "Buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq 'Buzz'
+  end 
+  it 'returns "Fizzbuzz for the number 15' do
+    expect(15.fizzbuzz).to eq "FizzBuzz"
+  end
 end
